cdc528050f1873dac632129c9cff4732118d1259
Tyler Gragg <robots@tylergragg.com>
2020-01-09 10:39:58 -0500

    Basic ball tracking based on color and shape

---
+++ track_ball.py
@@ -0,0 +1,78 @@
+import cv2
+import rospy
+import numpy as np
+
+from collections import deque
+from imutils.video import VideoStream
+import imutils
+
+# Color bounds
+colorLower = (29,86,6)
+colorUpper = (64,255,255)
+# Length of line path to draw
+pts = deque(maxlen=32)
+
+# Which video source
+vs = cv2.VideoCapture(0)
+
+while True:
+    # Grab Video Frame
+    frame = vs.read()
+    frame = frame[1]
+
+    # Resize, blur, convert
+    frame = imutils.resize(frame, width=600)
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    
+    # Create mask from colors
+    mask = cv2.inRange(hsv, colorLower, colorUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+    
+    # Find contours from mask and grab them
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+    
+    # If we have a contour, find the biggest, then make circle
+    if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+		# only proceed if the radius meets a minimum size
+        if radius > 10:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+        pts.appendleft(center)
+
+	# loop over the set of tracked points
+	for i in range(1, len(pts)):
+		# if either of the tracked points are None, ignore
+		# them
+		if pts[i - 1] is None or pts[i] is None:
+			continue
+ 
+		# otherwise, compute the thickness of the line and
+		# draw the connecting lines
+		thickness = int(np.sqrt(32 / float(i + 1)) * 2.5)
+		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+	# show the frame to our screen
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+    key = cv2.waitKey(1) & 0xFF
+ 
+	# if the 'q' key is pressed, stop the loop
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
